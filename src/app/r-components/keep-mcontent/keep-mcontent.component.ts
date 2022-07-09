b149025d034df60e3752f488f1a6dfc68ae23d48
Muno Gwata <muno.gwata@gmail.com>
2022-07-09 14:16:25 +0200

    added the tabs

--- src/app/r-components/keep-mcontent/keep-mcontent.component.ts
+++ src/app/r-components/keep-mcontent/keep-mcontent.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keep-mcontent.component.scss']
 })
 export class KeepMcontentComponent implements OnInit {
+savingsOpen = true;
+savingsClose = false;
+debtOpen = false;
+debtClose =true;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleSaveClose(){
+    this.savingsClose = false;
+    this.savingsOpen = true;
+    this.debtClose = true;
+    this.debtOpen= false;
+  }
+
+  toggleSaveOpen(){
+    this.savingsClose = true;
+    this.savingsOpen = false;
+    this.debtClose = false;
+    this.debtOpen = true;
+  }
+
+  toggleDebtOpen(){
+    this.savingsClose = false;
+    this.savingsOpen = true;
+    this.debtClose = true;
+    this.debtOpen= false;
+  }
+
+  toggleDebtClose(){
+    this.savingsClose = true;
+    this.savingsOpen = false;
+    this.debtClose = false;
+    this.debtOpen = true;
   }
 
 }
